60b3a7b49fe817f35f361b13992fcf7a990f7343
Sebastian Swahn <sebbe.swahn@hotmail.se>
2024-08-21 09:56:39 +0200

    additional styling of navbar

---
+++ ideas.md
@@ -0,0 +1,1 @@
+shadow on hover (listitems)
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,50 +4,35 @@ import { Link, NavLink } from "react-router-dom";
 export const Navbar = () => {
   return (
     <nav
-      className="bg-emerald-600 py-4 w-1/6 pt-20 h-screen fixed z-50 opacity-0 scale-95 hover:opacity-100 hover:scale-100 duration-500 ease-in-out transition-opacity"
+      className="bg-emerald-600 py-4 pt-20 h-screen fixed z-50 opacity-0 scale-95 hover:opacity-100 hover:scale-100 duration-500 ease-in-out transition-opacity"
       style={{ position: "fixed", zIndex: 1000 }}
     >
       {" "}
-      <div className=" max-w-[1100px] m-auto px-2 flex justify-between items-center">
-        <ul className="flex flex-col text-lg gap-x-8 gap-y-12">
-          <li>
-            <NavLink
-              className="text-white text-2xl [&.active]:underline"
-              to="/"
-            >
+      <div className="m-0 p-0 flex justify-between items-center w-full">
+        <ul className="flex flex-col text-lg w-full">
+          <li className="hover:bg-emerald-700 px-4 py-4">
+            <NavLink className="text-white text-2xl " to="/">
               Home
             </NavLink>
           </li>
-          <li>
-            <NavLink
-              className="text-white text-2xl [&.active]:underline"
-              to="/about"
-            >
+          <li className="hover:bg-emerald-700 px-4 py-4">
+            <NavLink className="text-white text-2xl " to="/about">
               About
             </NavLink>
           </li>
-          <li>
-            <NavLink
-              className="text-white text-2xl [&.active]:underline"
-              to="/portfolio"
-            >
+          <li className="hover:bg-emerald-700 px-4 py-4">
+            <NavLink className="text-white text-2xl " to="/portfolio">
               Portfolio
             </NavLink>
           </li>
-          <li>
-            <NavLink
-              className="text-white text-2xl [&.active]:underline"
-              to="/cv"
-            >
+          <li className="hover:bg-emerald-700 px-4 py-4">
+            <NavLink className="text-white text-2xl " to="/cv">
               CV
             </NavLink>
           </li>
 
-          <li>
-            <NavLink
-              className="text-white text-2xl [&.active]:underline"
-              to="/contact"
-            >
+          <li className="hover:bg-emerald-700 px-4 py-4">
+            <NavLink className="text-white text-2xl " to="/contact">
               Contact
             </NavLink>
           </li>
