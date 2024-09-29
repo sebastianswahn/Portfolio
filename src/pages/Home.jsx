b3dc8f5f88bd26e7cf4b32fd6c740f9585b22f0b
Sebastian Swahn <sebbe.swahn@hotmail.se>
2024-09-29 22:56:47 +0200

    trying to handle error with providing my own designed logo -- not fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../Components/header";
 import Footer from "../Components/footer";
+import Carousel from "../Components/carousel";
 
 function Home() {
   return (
     <div>
       <Header />
+      <Carousel />
       <Footer />
     </div>
   );
