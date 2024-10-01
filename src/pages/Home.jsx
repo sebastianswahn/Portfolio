a903d9ffd3a7a5816a2276003bdbf4d8d109e27f
Sebastian Swahn <sebbe.swahn@hotmail.se>
2024-10-01 03:00:24 +0200

    read on scss docs about the img float class

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "../Components/header";
 import Footer from "../Components/footer";
 import Carousel from "../Components/carousel";
+import Services from "../Components/services";
 
 function Home() {
   return (
     <div>
       <Header />
       <Carousel />
+      <Services />
       <Footer />
     </div>
   );
