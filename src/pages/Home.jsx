aaa2014e9d12161e88cd6510a4a81742bfc4e787
Sebastian Swahn <sebbe.swahn@hotmail.se>
2024-10-03 16:27:53 +0200

    casing issue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { Header } from "../Components/header";
-import Footer from "../Components/footer";
-import Carousel from "../Components/carousel";
-import Services from "../Components/services";
-import Portfolios from "../Components/portfolios";
+import Header from "../components/header";
+import Footer from "../components/footer";
+import Carousel from "../components/carousel";
+import Services from "../components/services";
+import Portfolios from "../components/portfolios";
 
 function Home() {
   return (
