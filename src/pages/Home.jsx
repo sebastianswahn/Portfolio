e6425a3efd86a0c409b2e217e3e4650506b979c0
Sebastian Swahn <sebbe.swahn@hotmail.se>
2024-10-03 16:15:25 +0200

    adjusting imports for production

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "../Components/header";
+import { Header } from "../Components/header";
 import Footer from "../Components/footer";
 import Carousel from "../Components/carousel";
 import Services from "../Components/services";
