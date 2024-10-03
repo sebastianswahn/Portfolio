a4ab83f09232933d9fa63b257eec10544cb4ad5d
Sebastian Swahn <sebbe.swahn@hotmail.se>
2024-10-03 16:09:35 +0200

    removed the component that had bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { RouteNotFound } from "./Pages/RouteNotFound";
 import Home from "./pages/Home";
 
 function App() {
@@ -12,9 +11,6 @@ function App() {
         <Switch>
           <Route exact path="/">
             <Home />
-          </Route>
-          <Route path="*">
-            <RouteNotFound />
           </Route>
         </Switch>
       </div>
